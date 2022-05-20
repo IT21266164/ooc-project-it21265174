0d8c42b43ed4c198ac32a6dd1570d4058a25ef2b
IT21266164 <100277543+IT21266164@users.noreply.github.com>
2022-05-21 00:41:56 +0530

    Create Feedback.h
    IT21266164 D.M.S Udayantha

---
+++ Feedback.h
@@ -0,0 +1,12 @@
+//IT21266164 D.M.S Udayantha
+// Feedback
+class Feedback
+{
+private:
+ char custName[40];
+ int FeedbackID;
+public:
+ Feedback(int fbID,char name);
+ void displayFeedback();
+ ~Feedback();
+};
